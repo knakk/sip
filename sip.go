990f3296c0a47cb37e3bbc663ab1c2fd6e4c8f14
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-11-16 14:16:24 +0100

    Expose message type

--- sip.go
+++ sip.go
@@ -41,6 +41,11 @@ func NewMessage(f msgType) Message {
 	}
 }
 
+// Type returns the message type.
+func (m Message) Type() msgType {
+	return m.typ
+}
+
 // AddField adds a field to the Message. If the field is not repeatable, it
 // will overwrite any existing value for the field.
 func (m Message) AddField(fs ...Field) Message {
